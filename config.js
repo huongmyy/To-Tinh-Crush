c9ff43329560e010366d2c8edf0684de13353e9e
huongmyy <62494785+huongmyy@users.noreply.github.com>
2021-07-15 15:48:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'An oiiii!',
+    introDesc: `Gửi Lê Văn An, em tên là Người Yêu An,
+    Em viết đơn này vì em muốn nói với anh rằng em yêu anh
+    Nếu đã đọc đến đây thì là...`,
+    btnIntro: 'Anh cũng yêu em',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🙆',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy 😍 ',
+    btnYes: 'Say như điếu đổ 😵',
+    btnNo: 'Không, vẫn tỉnh lắm 😈',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu ♥️',
+    mess: 'Em biết mà 🥰. Yêu Ann nhiều nhiều.😇😘',
+    messDesc: 'Đưa tay đây nào, mãi bên nhau bạn nhóooo',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //https://www.messenger.com/t/100006409308839 
 }
